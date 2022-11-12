aa6d66abaebfa852e0f27809ed639f5d1f449e3e
Boris <bobostaykov@gmail.com>
2022-11-12 15:08:17 +0100

    Add support for multiple tracked items

--- tracker.py
+++ tracker.py
@@ -10,30 +10,39 @@ import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
+ITEM_DIR_PREFIX = 'item_'
 CURRENT_PRICE_FILE = 'current_price.txt'
+LOG_FILE = 'log.log'
 
 
 def main():
     try:
-        config()
-        if len(argv) < 2 or len(argv) > 3:
+        item_id = argv[2]
+        item_dir = ITEM_DIR_PREFIX + item_id
+        config(item_dir)
+        if len(argv) < 3 or len(argv) > 4:
             usage()
         url = argv[1]
-        item_name, price = get_name_and_price(url)
-        compare_to_previous_price(url, item_name, price)
-        persist_price(price)
+        price = get_price(url)
+        compare_to_previous_price(url, item_id, price)
+        persist_price(item_dir, price)
     except Exception as e:
         log.error(e)
 
 
-def config():
+def config(item_dir):
+    """ Configures directories and files """
+
     load_dotenv()
-    Path(CURRENT_PRICE_FILE).touch(exist_ok=True)
-    log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', filename='log.log', level=log.INFO)
+    if not os.path.exists(item_dir):
+        os.mkdir(item_dir)
+    Path(os.path.join(item_dir, CURRENT_PRICE_FILE)).touch(exist_ok=True)
+    log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', filename=os.path.join(item_dir, LOG_FILE),
+                    level=log.INFO)
 
 
-def get_name_and_price(url):
-    """ Finds the current price as a float for the tracked item and its name """
+def get_price(url):
+    """ Returns the current price for the tracked item as a float """
 
     try:
         page = requests.get(url)
@@ -41,17 +50,16 @@ def get_name_and_price(url):
         # try once more
         page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    item_name = soup.find('h1', 'page-title').text
     price_with_currency = soup.find('p', {'class': 'product-new-price'}).text
     price_str = price_with_currency.split()[0]
     formatted_price_str = price_str.replace('.', '').replace(',', '.')
-    return item_name, float(formatted_price_str)
+    return float(formatted_price_str)
 
 
-def compare_to_previous_price(url, item_name, current_price):
+def compare_to_previous_price(url, item_id, current_price):
     """ Compares current price to previous one and send a notification if needed """
 
-    with open(CURRENT_PRICE_FILE, 'r') as file:
+    with open(os.path.join(ITEM_DIR_PREFIX + item_id, CURRENT_PRICE_FILE), 'r') as file:
         previous_price_str = file.read()
         if previous_price_str == '':
             log.info(f'First run, price {current_price}')
@@ -61,32 +69,28 @@ def compare_to_previous_price(url, item_name, current_price):
             log.info(f'Price {current_price}')
             return
         log.info(f'Price change: {previous_price} -> {current_price}')
-        if len(argv) != 3:
-            notify(item_name, url, previous_price, current_price)
+        if len(argv) != 4:
+            notify(item_id, url, previous_price, current_price)
         else:
-            boundary_price_str = argv[2]
+            boundary_price_str = argv[3]
             try:
                 boundary_price = float(boundary_price_str)
             except ValueError:
                 usage()
             if current_price < boundary_price:
-                notify(item_name, url, previous_price, current_price)
+                notify(item_id, url, previous_price, current_price)
 
 
-def notify(item_name, url, previous_price, current_price):
-    """ Sends an email about a price drop """
+def notify(item_id, url, previous_price, current_price):
+    """ Sends an email about a price change """
 
     sender_email = os.environ.get('SENDER_EMAIL')
     receiver_email = os.environ.get('RECEIVER_EMAIL')
     email_password = os.environ.get('EMAIL_PASSWORD')
-    text = f'''The price of item
-{item_name}
-went from {previous_price} to {current_price}!
-
-{url}'''
+    text = f'The price of item "{item_id}" went from {previous_price} to {current_price}!\n\n{url}'
     message = EmailMessage()
     message.set_content(text)
-    message['Subject'] = 'Price Drop Alert'
+    message['Subject'] = 'Price Change Alert'
     message['From'] = sender_email
     message['To'] = receiver_email
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
@@ -95,23 +99,24 @@ went from {previous_price} to {current_price}!
         log.info('Sending email notification')
 
 
-def persist_price(price):
+def persist_price(item_dir, price):
     """ Writes the current price to a file to compare later """
 
-    with open(CURRENT_PRICE_FILE, 'w') as file:
+    with open(os.path.join(item_dir, CURRENT_PRICE_FILE), 'w') as file:
         file.write(str(price))
 
 
 def usage():
     """ Shows program usage """
 
-    message = f'''Usage: {argv[0]} url [-p boundary_price]
+    message = f'''Usage: {argv[0]} url id [boundary_price]
     
 Emag price tracker
 
 Arguments:    
-  url               URL of item to track
-  -p boundary_price If set, will notify only if current price is below it. Else, will notify on every price drop.'''
+  url            URL of item to track
+  id             A string uniquely identifying the tracked item, in case there are multiple
+  boundary_price If set, will notify only if current price is below it. Else, will notify on every price drop.'''
 
     log.error(message)
     raise SystemExit(message)
